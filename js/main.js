a3d2d9b3081cc0bdc806dd3f2df3f673758c9468
sole <sole@mozilla.com>
2014-12-05 18:11:52 -0800

    ahh beautiful

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-console.log('hey there');
-document.body.appendChild(document.createTextNode('created on the fly'));
-
 var req = new XMLHttpRequest();
 req.open('get', 'build/data.json');
 req.responseType = 'json';
@@ -10,4 +7,17 @@ req.send();
 
 function onDataLoaded(e) {
 	console.log(this.response);
+	var main = document.querySelector('main');
+
+	var emails = this.response;
+	emails.forEach(function(email) {
+		var article = document.createElement('article');
+		main.appendChild(article);
+		var h2 = document.createElement('h2');
+		h2.innerHTML = email.title;
+		var div = document.createElement('div');
+		div.innerHTML = email.contents;
+		article.appendChild(h2);
+		article.appendChild(div);
+	});
 }
